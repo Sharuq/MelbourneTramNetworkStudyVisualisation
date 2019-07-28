943366b39798c0fcc0673243135c485e993564dc
Sharuq <md.sharuq@gmail.com>
2019-07-28 22:58:52 +1000

    server file

---
+++ server.R
@@ -0,0 +1,283 @@
+# server.R for manipulation and plotting
+
+#Loading libraries
+
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(highcharter)
+library(leaflet)
+library(leaflet.extras)
+library(ggthemes)
+library(ggfortify)
+library(datasets)
+library(markdown)
+
+
+
+shinyServer(function(input, output) {
+  
+  #Function to frequency per route with selected criteria
+  frequencyPlotting <- function(data){
+    
+    theme_set(theme_stata())
+    
+    k=ggplot(data,aes(x = route_short_name,y = frequency , group=1, text=   paste("Route Long Name: ", route_long_name))) +
+      geom_line(color="tomato3")+
+      geom_point()+
+      xlab("Route short name") + ylab("Frequency")+
+      theme(axis.text.x = element_text(angle=90, vjust=0.5,face ="bold",colour = "black"), 
+            axis.text.y = element_text(vjust=0.5,face ="bold",colour = "black"), 
+            axis.title.x = element_text(face ="bold",colour = "black"),
+            axis.title.y = element_text(face ="bold",colour = "black"))
+    
+    #intractive visualisation for hovering
+    gg<-ggplotly(k,tooltip = c("x", "y","text"))
+    gg$elementId <- NULL
+    gg
+    
+  }
+  
+  #Function to average load per route with selected criteria
+  loadPlotting <- function(data) {
+    
+    theme_set(theme_wsj())
+    
+    p<- ggplot(data) +
+      geom_bar(aes(x = route_short_name, y = Average_load,text = paste("Route Long Name: ", route_long_name)), 
+               stat="identity", position = "dodge", width = 0.7,fill='tomato3') +
+      xlab("Route Short Name") + ylab("Average Load")+
+      theme(axis.text.x = element_text(angle=90, vjust=0.5,face ="bold",colour = "black"), 
+            axis.text.y = element_text(vjust=0.5,face ="bold",colour = "black"), 
+            axis.title.x = element_text(face ="bold",colour = "black"),
+            axis.title.y = element_text(face ="bold",colour = "black"))
+    
+    #intractive visualisation for hovering
+    gg<-ggplotly(p,tooltip = c("x", "y","text"))
+    gg$elementId <- NULL
+    gg
+    
+  }
+  #Function for Highchart dual axis summarized plotting
+  combinedPlotting<-function(data){
+    
+    thm=hc_theme_handdrawn()
+    
+    h = highchart() %>% 
+      hc_yAxis_multiples(
+        list(lineWidth = 3, lineColor="#ec0030", title=list(text="Frequency")),
+        list(lineWidth = 3, lineColor='#400024', title=list(text="Average_load"))
+      ) %>% 
+      hc_add_series(data = data$frequency, color='#ec0030', type = "line",name='Frequency') %>%
+      hc_add_series(data = data$Average_load, color="#400024", type = "line", yAxis = 1,name='Average_load') %>% 
+      hc_xAxis(categories = data$route_short_name, title = list(text = "Route short name")) %>% hc_add_theme(thm)
+    
+    h
+  }
+  
+  #Funtion for plotting frequency of routes with selected criteria on leaflet with color palette
+  frequencyLeafletPlotting <- function(data) {
+    
+    
+    
+    pal <- colorNumeric(palette = "Reds",domain = data$frequency)
+    
+    
+    m=leaflet(data) %>%
+      setView(lat = -37.81862 , lng = 144.95199, zoom=12) %>%
+      addProviderTiles(providers$OpenStreetMap.BlackAndWhite, group="Dark")
+    for(i in unique(data$route_short_name)){
+      m=m%>%
+        addPolylines(data=data[data$route_short_name==i,],lat=~shape_pt_lat,lng=~shape_pt_lon, group=~shape_id, color = ~pal(frequency), weight = 3, opacity = 1,
+                     popup =  ~paste("Route short name: ", route_short_name, "<br>",
+                                     "Route long name: ", route_long_name, "<br>",
+                                     "Frequency: ", frequency),
+                     label =  ~route_short_name,
+                     highlightOptions = highlightOptions(
+                       color='#00ff00', weight = 3,
+                       opacity = 1, fillOpacity = 1,
+                       bringToFront = TRUE, sendToBack = TRUE))
+      
+    }
+    m=m%>%addLegend(
+      "topright",pal = pal, values = ~frequency, opacity = 1)
+    m
+    
+  }
+  #Funtion for plotting average load of routes with selected criteria on leaflet with color palette
+   loadLeafletPlotting <- function(data) {
+   
+        pal <- colorNumeric(
+        palette = "Oranges",
+        domain = data$Average_load
+      )
+      
+      m=leaflet(data) %>%
+        setView(lat = -37.81862 , lng = 144.95199, zoom=12) %>%
+        addProviderTiles(providers$OpenStreetMap.BlackAndWhite, group="Dark")
+      for(i in unique(data$route_short_name)){
+      m = m%>%
+          addPolylines(data=data[data$route_short_name==i,],lat=~shape_pt_lat,lng=~shape_pt_lon, group=~shape_id,color = ~pal(Average_load), weight = 3, opacity = 1,
+                       popup =  ~paste("Route short name: ", route_short_name, "<br>",
+                                       "Route long name: ", route_long_name, "<br>",
+                                       "Average Load: ", Average_load),
+                       label =  ~route_short_name,
+                       highlightOptions = highlightOptions(
+                         color='#00ff00', weight = 3,
+                         opacity = 1, fillOpacity = 1,
+                         bringToFront = TRUE, sendToBack = TRUE))
+        
+      }
+      m = m%>%addLegend(
+        "topright",pal = pal, values = ~Average_load, opacity = 1)
+      m
+      
+    }
+
+  #To print captions accordingly to the input
+   
+output$caption <- renderText({
+    paste("Average Load per Route During ", input$TimeP, "Period")
+  })
+
+output$fcaption <- renderText({
+    paste("Frequency per Route During ", input$TimePeriod, "Period ", input$DayType )
+  })  
+
+output$scaption <- renderText({
+    paste("Combined per Route During ", input$TimePer, "Period ")
+  })  
+
+
+#To plot average load on selected criteria
+output$loadPlot <- renderPlotly({ 
+    
+  if (input$TimeP == "AM PEAK") 
+    {loadPlotting(AMfinal_survey_data)}
+  else 
+  {loadPlotting(PMfinal_survey_data)}
+  
+})
+
+#To plot frequency per route on selected criteria
+output$frequenyPlot <- renderPlotly({ 
+  
+  if (input$TimePeriod == "AM PEAK") 
+    
+   { 
+    if(input$DayType=="Weekday")
+    {
+      frequencyPlotting(weekdayPM)
+    }
+    else if(input$DayType=="Sunday")
+    {
+      frequencyPlotting(sundayPM)
+    }
+    else if(input$DayType=="Saturday")
+    {
+      frequencyPlotting(saturdayPM)
+    }
+    
+  }
+  
+  else
+    
+  {
+    if(input$DayType=="Weekday")
+    {
+      
+      frequencyPlotting(weekdayAM)
+    }
+    else if(input$DayType=="Sunday")
+    {
+      
+      frequencyPlotting(sundayAM)
+    }
+    else if(input$DayType=="Saturday")
+    {
+    
+      frequencyPlotting(saturdayAM)
+    } 
+    
+  }
+  
+})
+
+#To plot frequency as color palette on leaflet
+output$freqencyMap <- renderLeaflet({
+  
+  if (input$TimePeriod == "AM PEAK") {
+   
+       if(input$DayType=="Weekday")
+        {
+         frequency_paths <- routes_shapes %>% left_join(weekdayAM, by = c("route_short_name" = "route_short_name", "route_long_name" = "route_long_name"))
+         
+        }
+        else if(input$DayType=="Sunday")
+        {
+          frequency_paths <- routes_shapes %>% left_join(sundayAM, by = c("route_short_name" = "route_short_name", "route_long_name" = "route_long_name"))
+          
+        }
+        else
+        {
+          frequency_paths <- routes_shapes %>% left_join(saturdayAM, by = c("route_short_name" = "route_short_name", "route_long_name" = "route_long_name"))
+          
+        }
+    
+    
+    frequencyLeafletPlotting(frequency_paths)
+  }
+  
+  else {
+        if(input$DayType=="Weekday")
+        {
+        frequency_paths <- routes_shapes %>% left_join(weekdayPM, by = c("route_short_name" = "route_short_name", "route_long_name" = "route_long_name"))
+        
+        }
+        else if(input$DayType=="Sunday")
+        {
+          frequency_paths <- routes_shapes %>% left_join(sundayPM, by = c("route_short_name" = "route_short_name", "route_long_name" = "route_long_name"))
+          
+        }
+        else
+        {
+          frequency_paths <- routes_shapes %>% left_join(saturdayPM, by = c("route_short_name" = "route_short_name", "route_long_name" = "route_long_name"))
+          
+        }
+    
+    
+    frequencyLeafletPlotting(frequency_paths)
+  }
+})
+
+#To plot average as color palette in leaflet
+output$loadMap <- renderLeaflet({
+  if (input$TimeP == "AM PEAK") {
+    AMload_paths<- routes_shapes %>% left_join(AMfinal_survey_data, by =c("route_short_name" = "route_short_name", "route_long_name" = "route_long_name"))
+    loadLeafletPlotting(AMload_paths)
+  }
+  else {
+    PMload_paths<- routes_shapes %>% left_join(PMfinal_survey_data, by = c("route_short_name" = "route_short_name", "route_long_name" = "route_long_name"))
+    loadLeafletPlotting(PMload_paths)
+  }
+})
+  
+#To plot summarised view on both averageload and frequency
+output$finalPlot <-renderHighchart({
+  
+  if (input$TimePer == "AM PEAK")
+  {
+    AMcombined<-weekdayAM %>% inner_join(AMfinal_survey_data, by = "route_short_name")
+    combinedPlotting(AMcombined)
+    
+  }
+  else
+  {
+    PMcombined<-weekdayAM %>% inner_join(PMfinal_survey_data, by = "route_short_name")
+    combinedPlotting(PMcombined)
+  }
+})
+})
